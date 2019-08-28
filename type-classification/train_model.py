cdbca51c3c56c126bdb6b96ff6f613edaeeefc66
monda <monda0524@gmail.com>
2019-08-28 21:25:50 +0900

    edit train_model.py

--- type-classification/train_model.py
+++ type-classification/train_model.py
@@ -13,26 +13,27 @@ def type_to_zero_one(types, type_to_one):
 def main():
     f = open('../data/pokemon_data.json')
     pokemon_json = json.load(f)
-    pokemon_df = pd.DataFrame(data=pokemon_json)
+    pokemon_df = json_normalize(data=pokemon_json)
 
     pokemon_df['type_zero_one'] = pokemon_df['types'].apply(type_to_zero_one, type_to_one='でんき')
 
-    print(pokemon_df.head())
-    X = json_normalize(data=pokemon_json, record_path='stats')
+    X = pokemon_df.iloc[:, 8:14]
     y = pokemon_df['type_zero_one']
-    print(X.head())
 
     # データを分ける
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
     # ロジスティック回帰
+    print("Logistic Regression ...")
     lr = LogisticRegression(C=1000)
 
     # 学習
+    print("training ... ")
     lr.fit(X_train, y_train)
 
     # スコアを表示
-    lr.score(X_test, y_test)
+    score = lr.score(X_test, y_test)
+    print("score is ", score)
 
 if __name__ == '__main__':
     main()
